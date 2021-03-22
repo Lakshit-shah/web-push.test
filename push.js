7048ebc7017e9dd3a3ab77cf79e3645a41efe40d
Lakshit Shah <lakshit.shrimankar@gmail.com>
2021-03-22 13:47:58 +0530

    fix

--- push.js
+++ push.js
@@ -6,8 +6,8 @@ const keysGenerated = {
 };
 
 
-webpush.setVapidDetails('Test Subject', keysGenerated.publicKey, keysGenerated.privateKey);
+webpush.setVapidDetails('mailto:lakshit.shrimankar@gmail.com', keysGenerated.publicKey, keysGenerated.privateKey);
 
-const sub = {};
+const sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/f4utFqt8m8U:APA91bHfuEOz8k4-EoQqlKkh9ffBMGhhXXxlvcEQ-vOoYd9Iygh3vjAXbfOVVlBDm5LqcHSPbd-I4Pr-0VZL8vSsGTA6rlfPSA4FJjbAxyd8cNjg880bPDGfNMB8-BilsCkXliD8avvu", "expirationTime": null, "keys": { "p256dh": "BAvQCXcotZW1PWqVjXqWWQcRp1aixgx5MJjtEEocE94VUA1ZVuw47Wm7BK5i1oeFqVJfICmt1NWqaDF9ZJLcXTM", "auth": "F2DTBsGaepkExfVl3yiszg" } };
 webpush.sendNotification(sub, 'test message');
 
